3a52ef34aec5d93ad849b90f6da2a19ea4e6f448
acatzk <38458781+acatzk@users.noreply.github.com>
2020-05-16 15:49:51 +0800

    changed the import into components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,15 +6,12 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import Navbar from "@/components/Navbar.vue";
-import Jumbotron from "@/components/Jumbotron.vue";
 
 export default {
   name: "Home",
   components: {
-    navbar: Navbar,
-    jumbotron: Jumbotron
+    navbar: () => import('@/components/Navbar.vue'),
+    jumbotron: () => import('@/components/Jumbotron.vue')
   }
 }
 </script>
