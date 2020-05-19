2683a7267ce83438f6426e38c682aa35576b72df
acatzk <joshuaimalay@gmail.com>
2020-05-19 13:05:18 +0800

    create apollo and setup connection

---
+++ src/apollo/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+
+import VueApollo from "vue-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const httpLink = new HttpLink({
+  uri: "https://graphql-data.herokuapp.com/v1/graphql",
+  headers: {
+    'x-hasura-admin-secret': 'ilusmdm'
+  }
+});
+
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+  
+  Vue.use(VueApollo);
+
+  const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+    defaultOptions: {
+      $loadingKey: "loading"
+    }
+  });
+
+  export default apolloProvider
