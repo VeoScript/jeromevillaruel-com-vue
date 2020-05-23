68f525f64eb55b475c4c97171b40152fad3f1b16
VeoScript <jeromevillaruel1998@gmail.com>
2020-05-23 12:46:55 +0800

    Freedomwall

--- src/views/Freedomwall.vue
+++ src/views/Freedomwall.vue
@@ -76,7 +76,7 @@
                 <b-card-text max-width="100">
                     {{ post.posts }}
                 </b-card-text>
-                <br><span class="countreact"><b-icon icon="heart-fill"></b-icon>{{ post.react.aggregate.count }}</span>&nbsp;
+                <br><span class="countreact"><b-icon icon="heart-fill">&nbsp;</b-icon>{{ post.react.aggregate.count }}</span>&nbsp;
                 <span class="countreact mx-2"><b-icon icon="chat-square-dots-fill"></b-icon> 0</span>
               </div>
               <hr>
