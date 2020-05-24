d242f930023895e778963f4df1988761c6e84b29
acatzk <joshuaimalay@gmail.com>
2020-05-24 11:04:17 +0800

    create MyInfoCard component

---
+++ src/components/MyInfoCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-card id="card-about" no-body class="overflow-hidden mb-2" style="max-width: 540px;">
+        <b-row no-gutters>
+            <b-col sm="6">
+            <b-card-img src="https://pbs.twimg.com/media/EYCOxmtUwAA__Hz?format=jpg&name=medium" alt="Image" class="rounded-0"></b-card-img>
+            </b-col>
+            <b-col sm="6">
+            <b-card-body title="Villaruel Jerome">
+                <b-card-text>
+                @veoscript/contact<br>
+                -09753286466<br>
+                -09380044523<br>
+                </b-card-text>
+            </b-card-body>
+            </b-col>
+        </b-row>
+    </b-card>
+</template>
