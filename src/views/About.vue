2ac104b9dea59b1e8af377ff145a32ca85baa1a0
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-05-27 15:19:24 +0800

    updated profile picture in about

--- src/views/About.vue
+++ src/views/About.vue
@@ -71,7 +71,22 @@
         </b-card>
       </b-col>
       <b-col cols="sm-4" class="mt-3">
-        <my-info-card /> <!-- My Information Card -->
+        <b-card id="card-about" no-body class="overflow-hidden mb-2" style="max-width: 540px;">
+          <b-row no-gutters>
+              <b-col sm="6">
+              <b-card-img :src="require('../assets/photos/veo.jpg')" alt="Image" class="rounded-0"></b-card-img>
+              </b-col>
+              <b-col sm="6">
+              <b-card-body title="Villaruel Jerome">
+                  <b-card-text>
+                  @veoscript/contact<br>
+                  -09753286466<br>
+                  -09380044523<br>
+                  </b-card-text>
+              </b-card-body>
+              </b-col>
+          </b-row>
+        </b-card>
         <b-card id="card-about" class="overflow-hidden" style="max-width: 540px;">
           <b-row>
             <b-col sm="12">
