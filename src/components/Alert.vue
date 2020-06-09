c26bde0e8a03a6a8b29736cc78e9bcc3c341b993
acatzk <joshuaimalay@gmail.com>
2020-06-09 11:11:27 +0800

    create alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-alert 
+        show 
+        dismissible
+        variant="danger"
+    >
+        {{ text }} <b>&rArr;</b>
+    </b-alert>
+</template>
+
+<script>
+export default {
+    name: 'Alert',
+
+    props: {
+        text: {
+            required: true,
+            type: String
+        }
+    }
+}
+</script>
