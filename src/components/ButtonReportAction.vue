d72cbff90dbee12d0813289e0a91f5c265eae5c4
acatzk <joshuaimalay@gmail.com>
2020-06-09 12:12:32 +0800

    added eye and eye-slash toggle in password

--- src/components/ButtonReportAction.vue
+++ src/components/ButtonReportAction.vue
@@ -25,14 +25,26 @@
                     label="Enter the administrator Password"
                     label-for="name-input"
                 >
-                    <b-form-input
-                        id="name-input"
-                        v-model.trim="$v.password.$model"
-                        autocomplete="off"
-                        :class="{ 'is-invalid' : $v.password.$error, 'is-valid' : !$v.password.$invalid }"
-                    ></b-form-input>
-                    <div class="invalid-feedback feedback">
-                        <span v-if="!$v.password.required">Password is required</span>
+                    <div class="input-group">
+                        <input 
+                            autocomplete="off"
+                            type="password" 
+                            id="password"
+                            class="form-control" 
+                            v-model.trim="$v.password.$model"
+                            :class="{ 'is-invalid' : $v.password.$error, 'is-valid' : !$v.password.$invalid }"
+                        >
+                        <div id="btnText" type="button" class="input-group-append" @click="toggleShowPassword" style="cursor: pointer;">
+                            <i v-if="!showpassword" class="input-group-text">
+                                <b-icon icon="eye-fill"></b-icon>
+                            </i>
+                            <i v-else class="input-group-text">
+                                <b-icon icon="eye-slash-fill"></b-icon>
+                            </i>
+                        </div>
+                        <div class="invalid-feedback feedback">
+                            <span v-if="!$v.password.required">Password is required</span>
+                        </div>
                     </div>
                 </b-form-group>
                 <b-button
@@ -84,7 +96,8 @@ export default {
             error: '',
             dismissSecs: 10,
             dismissCountDown: 0,
-            showDismissibleAlert: false
+            showDismissibleAlert: false,
+            showpassword: false,
         }
     },
 
@@ -116,6 +129,16 @@ export default {
                     this.error = 'Your password is absolutely invalid...'
                 }
             }
+        },
+        toggleShowPassword() {
+            let show = document.getElementById('password')
+            if(this.showpassword == false) {
+                this.showpassword = true
+                show.type = 'text'
+            } else {
+                this.showpassword = false
+                show.type = 'password'
+            }
         }
     },
 
@@ -130,3 +153,11 @@ export default {
 
 }
 </script>
+
+<style scoped>
+#password, #btnText{
+    background: #495DAC;
+    border: 0;
+    color: #ffffff;
+}
+</style>
