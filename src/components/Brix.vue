86212901547fc78d6ec41b143464f3f9ad3bd9a1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-07-03 14:56:11 +0800

    added brix component

--- src/components/Brix.vue
+++ src/components/Brix.vue
@@ -22,7 +22,7 @@
             We sang that song when we performed it every event in our school, and that song was so famous those days. <br>
             From the year of 2012 I learned Piano by my own and created a song <b>"Hulmahan"</b>, <br>
             dedicated to those people who maked broken promises. <br>
-            And now 2020 I'm continue making a songs in genre of Pop and Hiphop. <br>
+            And now I'm continue making a songs in genre of Pop and Hiphop. <br>
             Kindly support me in Underground Music Industry.
           </div>
         </b-col>
