1e82b6147a733b3adb4ab01851ff53c6b7069594
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-07-03 15:28:35 +0800

    changed to 100vh

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -55,7 +55,6 @@
     <b-container>
       <brix/>
     </b-container>
-    <BackToTop/>
     <foot-bar/>
   </div>
 </template>
@@ -143,6 +142,7 @@ export default {
     border-radius: 0px;
     color: #A8D1E7;
     margin-top: 65px;
+    height: 100vh;
   }
   .swal2-modal{
     background: #20284D;
